d3894ef3d57fc95b04a802388f5637121248d709
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-12-04 11:48:36 +0530

    create login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-page padding class="flex flex-center">
+    <q-card style="min-width:350px;">
+      <q-card-section>
+        <div>
+          <p class="text-h5 text-weight-bold q-mt-sm text-primary">
+            Welcome To Bellboy
+          </p>
+          <p class="text-body1">Please login to continue</p>
+        </div>
+        <form @submit.prevent.stop="login" class="q-gutter-sm">
+          <q-input
+            color="primary"
+            v-model="user.username"
+            label="Username"
+            type="username"
+            ref="input_username"
+            :rules="[val => !!val || 'Username is required']"
+            lazy-rules
+          />
+          <q-input
+            color="primary"
+            v-model="user.password"
+            label="Password"
+            type="password"
+            ref="input_password"
+            :rules="[val => !!val || 'Password is required']"
+            lazy-rules
+          />
+
+          <div>
+            <q-btn
+              label="Login"
+              type="submit"
+              color="primary"
+              class="full-width"
+            />
+          </div>
+        </form>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageLogin",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+
+  methods: {
+    login() {
+      this.$store.dispatch("auth/login", this.user);
+    }
+  }
+};
+</script>
