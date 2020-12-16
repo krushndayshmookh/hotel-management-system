6dc7affd99b82563cb8b6016b2b99207e3cb89a3
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-12-16 14:41:34 +0530

    reset local db on login

--- src/boot/nedb.js
+++ src/boot/nedb.js
@@ -26,6 +26,20 @@ Room.ensureIndex({ fieldName: "_id", unique: true, sparse: true });
 Floor.ensureIndex({ fieldName: "_id", unique: true, sparse: true });
 Booking.ensureIndex({ fieldName: "_id", unique: true, sparse: true });
 
-const db = { Guest, Room, Floor, Booking };
+async function resetWith(hotel) {
+  await Room.asyncRemove({}, { multi: true });
+  await Floor.asyncRemove({}, { multi: true });
+  await Booking.asyncRemove({}, { multi: true });
+
+  // await Room.persistence.compactDatafile();
+  // await Floor.persistence.compactDatafile();
+  // await Booking.persistence.compactDatafile();
+
+  await Room.asyncInsert(hotel.rooms);
+  await Floor.asyncInsert(hotel.floors);
+  await Booking.asyncInsert(hotel.bookings);
+}
+
+const db = { Guest, Room, Floor, Booking, resetWith };
 
 Vue.prototype.$db = db;
